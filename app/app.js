d1f9d3b3a4d593598e3dedb110f96ffb7d3114ec
Bishaka Samuel <sbishaka@gmail.com>
2016-01-30 10:35:44 +0300

    Secure build test

--- app/app.js
+++ app/app.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-  res.send("Hello World!.. It's Done");
+  res.send("Hello World!.. It's Done and now secure!");
 });
 
 app.listen(3000, function () {
